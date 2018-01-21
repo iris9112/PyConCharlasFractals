30f33be512b36a6c747f3d0e948919ce7e5509ae
iris9112 <iris9112@gmail.com>
2018-01-20 23:38:08 -0500

    initial commit

---
+++ Cantor.py
@@ -0,0 +1,15 @@
+def cantor_set(x,y,l):
+    if l > 1: #keep doing this while the line is > 1 pixel lon
+        win.create_line(x,y,x+l,y) #draw horizontal lin
+        y = y + 50 #move 50 pixels down for next generation
+        cantor_set(x,y,l/3) #left hand offspring
+        cantor_set(x+2/3*l,y,l/3) #right hand offspring
+
+from tkinter import Tk, Canvas
+w,h = 750,250
+win = Canvas(Tk(),width=w,height=h)
+win.pack()
+cantor_set(10,10,w-20)
+
+
+win.mainloop()
